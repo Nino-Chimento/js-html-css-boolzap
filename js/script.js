202d0dcdb9e69bdec118152e1ba7607212dd2eab
Nino Chimento <saninoc84@gmail.com>
2020-01-23 20:26:27 +0100

    js cancellato ho conservato solo la parte che invia i messaggi

--- js/script.js
+++ js/script.js
@@ -26,35 +26,5 @@ $(document).ready(function () {
       messaggioRicevuto.children("p").text("ok");
       $(".conversazione").append(messaggioRicevuto);
     }, 1000);
+    });
   });
-  // cerca utente
-  $(".cerca-utente").keypress(function () {
-    if(event.which == 13 || event.keyCode == 13) {
-      var ricerca = $(".cerca-utente").val();
-      console.log(ricerca);
-      numeroElementi = $(".wrap-menu-utenti li").length
-      for (var i = 0; i < $(".wrap-menu-utenti li").length; i++) {
-        console.log($(".lista-utenti li").attr("data-element"));
-      }
-    }
-  });
-
-  // cancellazione messaggi
-  $(document).on("click",".fa-angle-down",function () {
-      $(this).next().toggleClass("display-none");
-      $(".delete").click(function () {
-        $(this).parent("contenitore-messaggio-inviato").remove();
-      });
-    console.log("ni");
-  })
-  $(".fa-angle-down").click(function () {
-    $(this).next().toggleClass("display-none");
-    $(".delete").click(function () {
-      console.log("N");
-      $(this).parents(".contenitore-messaggio-inviato").remove();
-  });
-
-
-  });
-
-});
