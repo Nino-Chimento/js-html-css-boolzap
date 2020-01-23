884166427d1b93d3fe414f1083e20594150526db
Nino Chimento <saninoc84@gmail.com>
2020-01-23 14:34:39 +0100

    inviato i messaggi

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     if(event.which == 13 || event.keyCode == 13) {
         var testo = $("#invio").val();
         console.log(testo);
-        var nuovoElemento = $("copia").clone();
-        console.log(nuovoElemento);
-        $("conversazione").append(nuovoElemento);
-    }
+        var nuovoElemento = $(".copia").clone();
+        nuovoElemento.find("p").text(testo);
+        $(".conversazione").append(nuovoElemento);
+      }
+    })
   })
-})
