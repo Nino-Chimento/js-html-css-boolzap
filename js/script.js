ba94350deb1c6b89db4cd5e8e4fd185cd6ed7f7f
Nino Chimento <saninoc84@gmail.com>
2020-01-24 07:28:37 +0100

    cancello i messaggi che invio e non presenti nell html originale

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $("#invio").keypress(function () {
     if(event.which == 13 || event.keyCode == 13) {
         var testo = $("#invio").val();
-        console.log(testo);
         var nuovoElemento = $(".template .copia").clone();
         nuovoElemento.find("p").text(testo);
         $("#invio").val("");
@@ -14,21 +13,28 @@ $(document).ready(function () {
         }, 1000);
       }
     })
-  $(".fa-telegram-plane").click(function () {
-    var testo = $("#invio").val();
-    console.log(testo);
-    var nuovoElemento = $(".template .copia").clone();
-    nuovoElemento.find("p").text(testo);
-    $(".conversazione").append(nuovoElemento);
-    $("#invio").val("");
-    setTimeout(function () {
-      var messaggioRicevuto = $(".messaggio-ricevuto").clone();
-      messaggioRicevuto.children("p").text("ok");
-      $(".conversazione").append(messaggioRicevuto);
-    }, 1000);
+    $(".fa-telegram-plane").click(function () {
+      var testo = $("#invio").val();
+      var nuovoElemento = $(".template .copia").clone();
+      nuovoElemento.find("p").text(testo);
+      $(".conversazione").append(nuovoElemento);
+      $("#invio").val("");
+      setTimeout(function () {
+        var messaggioRicevuto = $(".messaggio-ricevuto").clone();
+        messaggioRicevuto.children("p").text("ok");
+        $(".conversazione").append(messaggioRicevuto);
+      }, 1000);
+    });
+    $(".fa-angle-down").click(function () {
+      $(this).next().toggleClass("display-none");
+    });
+    $(".delete").click(function () {
+      $(this).parents(".contenitore-messaggio-inviato").remove();
+    });
+    $(document).on("click",".fa-angle-down",function () {
+      $(this).next().toggleClass("display-none");
+    });
+    $(document).on("click",".delete",function () {
+      $(this).parents(".contenitore-messaggio-inviato").remove();
+    })
   });
-  $(".fa-angle-down").click(function () {
-    console.log("n");
-    $(this).next().toggleClass("display-none");
-  });
-});
